88cd2c20e70e6835e114f27b453e124e5186cbdf
IT25102116 <it25102116@my.sliit.lk>
2026-09-22 23:49:45 +0530

    IT25102116Lab5Q2

---
+++ Q2/IT25102116Lab5Q2.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class IT25102116Lab5Q2 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter number of new members: ");
+        int members = input.nextInt();
+
+        if (members < 0) {
+            System.out.println("Invalid number of members");
+            return;
+        }
+
+        switch (members) {
+            case 0:
+                System.out.println("Prize: No Prize");
+                break;
+
+            case 1:
+                System.out.println("Prize: Pen");
+                break;
+
+            case 2:
+                System.out.println("Prize: Umbrella");
+                break;
+
+            case 3:
+                System.out.println("Prize: Bag");
+                break;
+
+            case 4:
+                System.out.println("Prize: Travelling Chair");
+                break;
+
+            default:
+                System.out.println("Prize: Headphone");
+        }
+    }
+}
